265538dab81e1056d1409feff63f70dac97a003a
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-05-31 20:09:06 -0700

    Added Pages

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+//configuration:
+require('dotenv').config()
+const PORT = process.env.PORT
+console.log(PORT)
+
+  
+// Breads:
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+//routes:
+app.get('/', (req, res) => {
+    res.send('Welcome to the World of Bread!')
+})
+  
+
+app.listen(PORT, ()=> {
+    console.log('listening on port: ', PORT)
+})
