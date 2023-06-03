e99e9868ad489f153128786336fb64dc5b64c981
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-06-03 09:57:59 -0700

    first step day 4

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,9 +11,12 @@ breads.get('/', (req, res) => {
 })
 
 
-// SHOW
+// SHOW Route
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+    // res.send(Bread[req.params.arrayIndex])
+    res.render('Show', {
+      bread: Bread[req.params.arrayIndex]
+    })
   })
 
 module.exports = breads
