e91b781aa65d3f290a2400943e0b3dedaf582992
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-06-03 14:33:04 -0700

    Add bread function working, gluten result working

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -10,6 +10,11 @@ breads.get('/', (req, res) => {
   // res.send(Bread)
 })
 
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
+})
+
 
 // SHOW Route
 breads.get('/:arrayIndex', (req, res) => {
@@ -21,6 +26,21 @@ breads.get('/:arrayIndex', (req, res) => {
   }else{
     res.send('404')
   }
+  })
+
+  //Create
+  breads.post('/', express.urlencoded({ extended: true }),(req, res)=>{
+    // console.log(req.body)
+     if (!req.body.image) {
+      req.body.image = 'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=400&q=80'
+     }
+    if(req.body.hasGluten === 'on'){
+      req.body.hasGluten = 'true'
+    } else {
+      req.body.hasGluten = 'false'
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
   })
 
 module.exports = breads
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
