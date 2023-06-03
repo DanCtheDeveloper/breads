5931c4eec615570264d861e87db8e4fe645068e7
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-06-03 10:39:49 -0700

    Links and Images Working, 404 Page added

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
-  
-// Breads/middleware:
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
 //routes:
 app.get('/', (req, res) => {
     res.send('Welcome to the World of Bread!')
 })
   
+// Breads/middleware:
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+  
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
+
+
 //Adding Comment Checking Git
 
 app.listen(PORT, ()=> {
--- views/show.jsx
+++ views/show.jsx
@@ -1,11 +1,14 @@
 const React = require('react')
-const Default = require('./layout/Default')
+const Default = require('./layout/default')
 
 function Show ({bread}) {
   console.log(bread.name)
     return (
       <Default>
         <h2>Show Page</h2>
+        <h3>{bread.name}</h3>
+        <img src={bread.image} alt={bread.name}/>
+        <li><a href="/breads">Go Home</a> </li>
       </Default>
     )
 }
