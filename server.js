99b03ebb1e29dd1b13722ac60e16eea62e543128
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-06-24 10:46:23 -0700

    Changed server.js

--- server.js
+++ server.js
@@ -2,51 +2,41 @@ const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
-
-//configuration:
+// CONFIGURATION
 require('dotenv').config()
-const PORT = process.env.PORT
-console.log(PORT)
 
-//routes:
-app.get('/', (req, res) => {
-    res.send('Welcome to the World of Bread!')
+app.get('/', (req,res) =>{
+    res.send('Welcome to an Awesome Bread App')
 })
 
-mongoose.connect(process.env.MONGO_URI,
-    {useNewUrlParser: true, useUnifiedTopology: true},
-    ()=>{console.log('connected to mongoDB: ', process.env.MONGO_URI)})
 
-    
-    
-    
-    
-    // Middleware:
-    app.use(methodOverride('_method'))
-    app.use(express.static('public'))
-    app.use(express.urlencoded({extended:true}))
-    app.set('views', __dirname + '/views')
-    app.set('view engine', 'jsx')
-    app.engine('jsx', require('express-react-views').createEngine())
-    
-    //Breads
-    const breadsController = require('./controllers/breads_controller.js')
-    app.use('/breads', breadsController)
-    
-// bakers 
-const bakersController = require('./controllers/bakers_controller.js')
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+
+//BREADS
+const breadsController = require('./controllers/breads_controller')
+app.use('/breads', breadsController)
+
+//BAKERS
+const bakersController = require('./controllers/bakers_controller')
 app.use('/bakers', bakersController)
-  
+
+const PORT = process.env.PORT || 5000
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log('connected to : ', process.env.MONGO_URI)}
+    )
 // 404 Page
 app.get('*', (req, res) => {
     res.send('404')
   })
-  
 
-
-//Adding Comment Checking Git
-
-app.listen(PORT, ()=> {
-    console.log('listening on port: ', PORT)
-})
-// https://digitalskills.instructure.com/courses/9255/pages/code-along-be-3-server-side-views-and-rendering-with-jsx?module_item_id=1307340#
+app.listen(process.env.PORT)
+// app.listen(PORT, () =>{
+//     console.log('LISTENING ON SERVER', PORT)
+// })
